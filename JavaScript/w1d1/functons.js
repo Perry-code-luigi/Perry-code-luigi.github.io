c95bbba0461b572b3ca8bc4dcfc8fe772a538c48
unknown <perryluigi16@gmail.com>
2020-04-28 20:59:15 -0500

    done with the homework

--- JavaScript/w1d1/functons.js
+++ JavaScript/w1d1/functons.js
@@ -39,11 +39,11 @@ function filterLongWords(str,i){
 
 
 
-console.log("Expected sum output for arr[2,2,3]: 6\n" + myFunctionTest(sum([2,2,3]), 7));
-console.log("Expected sum output for arr[2,2,3]: 6\n" + myFunctionTest(sum([2,2,3]), 3));
-console.log("Expected multiply output for arr[2,2,3]: 12\n" + myFunctionTest(multiply([2,2,3]), 12));
-console.log("Expected multiply output for arr[2,2,3]: 12\n" + myFunctionTest(multiply([2,2,3]), 7));
-console.log("Expected reverse result for perry: yrrep\n" + myFunctionTest(reverse("perry"), "yrrep"));
-console.log("Expected reverse result for perry: yrrep\n" + myFunctionTest(reverse("perry"), "rep"));
-console.log("Expected word length greater than 3 inside ['bird','dog','monkey']: ['bird','monkey']\n" + myFunctionTest(filterLongWords(['bird','dog','monkey'],3), JSON.stringify(['bird','monkey'])));
-console.log("Expected word length greater than 3 inside ['bird','dog','monkey']: ['bird','monkey']\n" + myFunctionTest(filterLongWords(['bird','dog','monkey'],3), JSON.stringify(['aaa','bbb'])));
+// console.log("Expected sum output for arr[2,2,3]: 6\n" + myFunctionTest(sum([2,2,3]), 7));
+// console.log("Expected sum output for arr[2,2,3]: 6\n" + myFunctionTest(sum([2,2,3]), 3));
+// console.log("Expected multiply output for arr[2,2,3]: 12\n" + myFunctionTest(multiply([2,2,3]), 12));
+// console.log("Expected multiply output for arr[2,2,3]: 12\n" + myFunctionTest(multiply([2,2,3]), 7));
+// console.log("Expected reverse result for perry: yrrep\n" + myFunctionTest(reverse("perry"), "yrrep"));
+// console.log("Expected reverse result for perry: yrrep\n" + myFunctionTest(reverse("perry"), "rep"));
+// console.log("Expected word length greater than 3 inside ['bird','dog','monkey']: ['bird','monkey']\n" + myFunctionTest(filterLongWords(['bird','dog','monkey'],3), JSON.stringify(['bird','monkey'])));
+// console.log("Expected word length greater than 3 inside ['bird','dog','monkey']: ['bird','monkey']\n" + myFunctionTest(filterLongWords(['bird','dog','monkey'],3), JSON.stringify(['aaa','bbb'])));
